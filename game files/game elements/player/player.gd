1f9cb9ae3ddf128cc81cf3fdb99c057840d9d687
i1rs7 <163767002+i1rs7@users.noreply.github.com>
2025-03-15 22:49:33 -0400

    added player art

--- game files/game elements/player/player.gd
+++ game files/game elements/player/player.gd
@@ -22,12 +22,20 @@ func move(delta: float) -> void:
 	if not is_on_floor(): velocity += get_gravity() * delta # Add the gravity.
 	if self.get_meta("selected"): # only evaluate movement if the node is selected
 		if Input.is_action_just_pressed("ui_up") and is_on_floor(): velocity.y = JUMP_VELOCITY # Handle jump.
+<<<<<<< Updated upstream
 		if Input.get_axis("ui_left","ui_right"): 
 			velocity.x = Input.get_axis("ui_left","ui_right") * SPEED # move based on left and right
 			if Input.get_axis("ui_left","ui_right") == 1: #handling animations
 				$cat.play("move_right")
 			if Input.get_axis("ui_left","ui_right") == -1:
 				$cat.play("move_left")
+=======
+		if Input.get_axis("ui_left","ui_right"): velocity.x = Input.get_axis("ui_left","ui_right") * SPEED # move based on left and right
+		if Input.get_axis("ui_left","ui_right") ==1: #animation right
+			$cat.play("move_right")
+		if Input.get_axis("ui_left","ui_right") ==-1: #animation left
+			$cat.play("move_left")
+>>>>>>> Stashed changes
 	move_and_slide() # Move by velocity.
 
 func handle_collisions():
