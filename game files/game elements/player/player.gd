bc64bc065532950b18c3f17d25a79c0b4250ce29
JacobKranzProjects <jacobgkranz@gmail.com>
2025-03-16 00:24:24 -0400

    added a bunch of stuff for merges
    see discord message. It doesnt work, and theres a lot of excess code. will make this a separate branch so it doesnt break stuff

--- game files/game elements/player/player.gd
+++ game files/game elements/player/player.gd
@@ -4,9 +4,19 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @onready var text = $PanelContainer/Label
+@onready var ancestor = self # only stays self for parent branch
+# These dictionaries must have the same keys. changes is strings, and attributes is vals. must be in same order as well
 var attributes = {
 	"keys": 0
 }
+var changes = {
+	"keys": ""
+}
+
+func get_nth_ancestor(n):
+	if n == 0: return self
+	else: return ancestor.get_nth_ancestor(n-1)
+
 
 func _on_ready() -> void:
 	set_meta("selected", false)
@@ -31,8 +41,10 @@ func handle_collisions():
 	for index in get_slide_collision_count(): # for every collision:
 		var collider = get_slide_collision(index).get_collider() # get the object collided with
 		if collider.name == "Key": # if its a key,
-			collider.queue_free() # destroy the key
+			collider.free() # destroy the key
 			attributes["keys"] += 1 # add a key to data
+		if Input.is_action_pressed("merge") and collider.is_in_group("players"):
+			get_parent().merge(self, collider)
 
 
 func update_text():
--- game files/game elements/player/player_manager.gd
+++ game files/game elements/player/player_manager.gd
@@ -18,14 +18,46 @@ func add_branch():
 	var branch = player_scene.instantiate() # create instance
 	branch.attributes = players[selected_index].attributes.duplicate() # copy attributes over
 	branch.position = players[selected_index].position + Vector2(20,0) # move new player to desired position
+	branch.ancestor = players[selected_index] # set ancestor for merge evaluation
 	players.append(branch) # add player ID
-	# note that we must add child before setting meta, I think because the meta is on the node, and not the object or something.
 	add_child(branch) # add new player to children
-	players[selected_index].set_meta("selected", false) # deselect current player
-	selected_index = players.size() - 1 # update selected index to be new player
-	players[selected_index].set_meta("selected", true) # select new player
+	
+	# uncomment this code if you want to select the new branch on creation
+	# note that we must add child before setting meta, I think because the meta is on the node, and not the object or something.
+	#players[selected_index].set_meta("selected", false) # deselect current player
+	#selected_index = players.size() - 1 # update selected index to be new player
+	#players[selected_index].set_meta("selected", true) # select new player
 
 func cycle_selection():
 	players[selected_index].set_meta("selected", false) # deselect current player
 	selected_index = (selected_index + 1) % players.size() # cycle index
 	players[selected_index].set_meta("selected", true) # select new player
+
+
+func find_common_ancestor(a, b) -> Node2D:
+	for i in get_child_count():
+		for j in i:
+			if a.get_nth_ancestor(i) == b.get_nth_ancestor(j):
+				return a.get_nth_ancestor(i)
+	return 
+		
+		
+
+func merge(kept_body, merged_body):
+	var new_attributes = kept_body.attributes.duplicate()
+	for key in kept_body.attributes:
+		if kept_body.attributes[key] != merged_body.attributes[key]:
+			var ancestor = find_common_ancestor(kept_body, merged_body)
+			print(ancestor)
+
+	
+	
+	if merged_body.get_meta("selected"): # if we are merging the selected body, we must select a new one
+		merged_body.set_meta("selected", false) # update metas
+		kept_body.set_meta("selected", true)
+	players.erase(merged_body) # remove merged body from list
+	for index in players.size(): # now we will just loop through the players and update selected index based on meta data. its o(n) but we shouldn't have too many objects and im too lazy to deal with edge cases rn 
+		if players[index].get_meta("selected"): # if its selected in metadata,
+			selected_index = index # update selected index
+			break # no need to continue
+	merged_body.free() # lastly, we delete the merged body
