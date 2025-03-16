efb388d3f1f1027e483254014485875b997769c0
i1rs7 <163767002+i1rs7@users.noreply.github.com>
2025-03-15 22:38:09 -0400

    Revert "added player art"

--- game files/game elements/player/player.gd
+++ game files/game elements/player/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -600.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 @onready var text = $PanelContainer/Label
 var attributes = {
 	"keys": 0
@@ -17,18 +17,15 @@ func _physics_process(delta: float) -> void:
 	handle_collisions()
 	update_text()
 
+
 func move(delta: float) -> void:
 	velocity.x = 0 # don't move x if not selected, set movement in selected if loop
 	if not is_on_floor(): velocity += get_gravity() * delta # Add the gravity.
 	if self.get_meta("selected"): # only evaluate movement if the node is selected
 		if Input.is_action_just_pressed("ui_up") and is_on_floor(): velocity.y = JUMP_VELOCITY # Handle jump.
-		if Input.get_axis("ui_left","ui_right"): 
-			velocity.x = Input.get_axis("ui_left","ui_right") * SPEED # move based on left and right
-			if Input.get_axis("ui_left","ui_right") == 1: #handling animations
-				$cat.play("move_right")
-			if Input.get_axis("ui_left","ui_right") == -1:
-				$cat.play("move_left")
+		if Input.get_axis("ui_left","ui_right"): velocity.x = Input.get_axis("ui_left","ui_right") * SPEED # move based on left and right
 	move_and_slide() # Move by velocity.
+
 
 func handle_collisions():
 	for index in get_slide_collision_count(): # for every collision:
@@ -44,4 +41,3 @@ func update_text():
 		new_text += attribute + ": " + str(attributes[attribute]) + "\n" # add the attribute name and value
 	new_text.strip_edges() # remove leading and trailing whitespace
 	text.text = new_text # set label text to placeholder text
-
