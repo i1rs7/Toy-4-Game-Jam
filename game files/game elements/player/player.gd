559e341fa60ab22693a2015324c11c7e98aa3192
JacobKranzProjects <jacobgkranz@gmail.com>
2025-03-15 20:44:50 -0400

    finished duplication and added key
    added key folder with associated scene
    added an attributes dictionary to player script and copy it when splitting
    set new branch to be directly to the right of old one
    also added label with backpanel to player to see the attributes, with a update text function to go along with it

--- game files/game elements/player/player.gd
+++ game files/game elements/player/player.gd
@@ -3,7 +3,10 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+@onready var text = $PanelContainer/Label
+var attributes = {
+	"keys": 0
+}
 
 func _on_ready() -> void:
 	set_meta("selected", false)
@@ -11,6 +14,8 @@ func _on_ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	move(delta)
+	handle_collisions()
+	update_text()
 
 
 func move(delta: float) -> void:
@@ -20,3 +25,19 @@ func move(delta: float) -> void:
 		if Input.is_action_just_pressed("ui_up") and is_on_floor(): velocity.y = JUMP_VELOCITY # Handle jump.
 		if Input.get_axis("ui_left","ui_right"): velocity.x = Input.get_axis("ui_left","ui_right") * SPEED # move based on left and right
 	move_and_slide() # Move by velocity.
+
+
+func handle_collisions():
+	for index in get_slide_collision_count(): # for every collision:
+		var collider = get_slide_collision(index).get_collider() # get the object collided with
+		if collider.name == "Key": # if its a key,
+			collider.queue_free() # destroy the key
+			attributes["keys"] += 1 # add a key to data
+
+
+func update_text():
+	var new_text = "" # create placeholder string to change instead of changing label text
+	for attribute in attributes:
+		new_text += attribute + ": " + str(attributes[attribute]) + "\n" # add the attribute name and value
+	new_text.strip_edges() # remove leading and trailing whitespace
+	text.text = new_text # set label text to placeholder text
