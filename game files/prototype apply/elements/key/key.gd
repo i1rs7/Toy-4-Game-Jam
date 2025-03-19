b4adc1c3e70c21a8dc223903e819bdd50b1aeaca
i1rs7 <163767002+i1rs7@users.noreply.github.com>
2025-03-19 21:34:14 +0100

    button animation, key above head

--- game files/prototype apply/elements/key/key.gd
+++ game files/prototype apply/elements/key/key.gd
@@ -1,4 +1,4 @@
-extends Control
+extends StaticBody2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,8 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("pause"):
-		show()
-
-#func resume():
-	#self.hide()
+	pass
--- game files/prototype apply/player/player types/player.gd
+++ game files/prototype apply/player/player types/player.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
+const KEY = preload("res://game files/prototype apply/elements/key/key.tscn")
+
 const SPEED = 600.0
 const JUMP_VELOCITY = -700.0
 const TRAMP_BOUNCE_VELOCITY = -950.0
 
+var player_has_key = false
+var player_key_above_head = false 
+
 func _on_ready() -> void:
-	#get_child(0).shape.radius = get_parent().radius
-	#get_child(1).mesh.radius = get_parent().radius
-	#get_child(1).mesh.height = 2*get_parent().radius
 	pass
 
 
@@ -15,6 +17,8 @@ func _on_ready() -> void:
 func _physics_process(delta: float) -> void:
 	move(delta)
 	handle_collisions()
+	if get_meta("key"):
+		self.get_node("key").visible = true
 
 
 func move(delta: float) -> void:
@@ -22,12 +26,7 @@ func move(delta: float) -> void:
 	if self.get_meta("selected"): # only evaluate movement if the node is selected
 		if Input.is_action_just_pressed("ui_up") and is_on_floor(): velocity.y = JUMP_VELOCITY # Handle jump.
 		velocity.x = Input.get_axis("ui_left","ui_right") * SPEED # move based on left and right
-		if Input.get_axis("ui_left","ui_right") == 1:
-			$AnimatedSprite2D.play("move_right")
-		elif Input.get_axis("ui_left","ui_right") == -1:
-			$AnimatedSprite2D.play("move_left")
-		elif Input.get_axis("ui_left","ui_right") == 0:
-			$AnimatedSprite2D.play("idle")
+		player_animation()
 	move_and_slide() # Move by velocity.
 
 
@@ -39,6 +38,7 @@ func handle_collisions():
 		if collider.is_in_group("doors") and get_meta("key"):
 			collider.state = false
 			set_meta("key", false)
+			self.get_node("key").visible = false
 		elif collider.is_in_group("keys"):
 			collider.queue_free()
 			set_meta("key", true)
@@ -47,8 +47,19 @@ func handle_collisions():
 			collider.play_animation()
 			
 			
+func player_animation():
+	if Input.get_axis("ui_left","ui_right") == 1:
+		$AnimatedSprite2D.play("move_right")
+	elif Input.get_axis("ui_left","ui_right") == -1:
+		$AnimatedSprite2D.play("move_left")
+	elif Input.get_axis("ui_left","ui_right") == 0:
+		$AnimatedSprite2D.frame = 0
 
 
+func set_key_position(key):
+	key.position.x = self.position.x/20
+	key.position.y = self.position.y/20 -25
+	
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_class("CharacterBody2D") and self != body:
 		get_parent().merge()
